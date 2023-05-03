61e00420fa675ae18830085585cf9ebac7162786
machearn <machearn.n@gmail.com>
2023-05-02 22:59:48 -0400

    feat(config): add config loader

---
+++ util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config is a struct that holds the configuration for the application.
+type Config struct {
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(configPath string) (Config, error) {
+	var config Config
+	v := viper.New()
+	v.AddConfigPath(configPath)
+	v.SetConfigName("app")
+	v.SetConfigType("env")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return config, err
+	}
+
+	if err := v.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
